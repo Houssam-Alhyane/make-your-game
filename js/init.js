d1d7e8d3b5116ba1e6d0470c97c32dc3ba1d99ac
halhyane <houssamalhyan5@gmail.com>
2026-06-14 10:42:52 +0100

    add init

--- js/init.js
+++ js/init.js
@@ -1,6 +1,25 @@
 import { BrickGenerator } from "./bricks.js"
 
+export function init(state) {
+    cancelAnimationFrame(state.raf)
+    state.waiting    = true
+    state.dx         = 4
+    state.dy         = -4
+    state.arrowLeft  = false
+    state.arrowRight = false
 
+    state.scoreEl.innerHTML = '0'
+    state.ball.style.cssText    = ''
+    state.breaker.style.cssText = ''
+
+    for (const b of [...document.getElementsByClassName('brick')]) b.remove()
+    state.layers = BrickGenerator()
+
+    state.home.style.display = 'none'
+    state.over.style.display = 'none'
+    state.game.style.display = 'flex'
+    state.prompt.style.display = 'flex'
+}
 
 export function resetBall(state) {
     state.ball.style.cssText    = ''
