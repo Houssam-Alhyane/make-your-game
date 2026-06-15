f8cdbab8777503186ede503f86b1abd33ab18ff3
HOUSSAM AL HYANE <alhyanehoussam@gmail.com>
2026-06-15 11:22:08 +0100

    Refactor init and resetBall functions for randomness

--- js/init.js
+++ js/init.js
@@ -1,31 +1,33 @@
-import { BrickGenerator } from "./bricks.js"
+import { BrickGenerator } from './bricks.js';
 
 export function init(state) {
-    cancelAnimationFrame(state.raf)
-    state.waiting    = true
-    state.dx         = 4
-    state.dy         = -4
-    state.arrowLeft  = false
-    state.arrowRight = false
+  cancelAnimationFrame(state.raf);
+  state.waiting = true;
+  const arr = [4, -4];
+  state.dx = arr[Math.floor(Math.random() * arr.length)];
+  state.dy = arr[Math.floor(Math.random() * arr.length)];
+  state.arrowLeft = false;
+  state.arrowRight = false;
 
-    state.scoreEl.innerHTML = '0'
-    state.ball.style.cssText    = ''
-    state.breaker.style.cssText = ''
+  state.scoreEl.innerHTML = '0';
+  state.ball.style.cssText = '';
+  state.breaker.style.cssText = '';
 
-    for (const b of [...document.getElementsByClassName('brick')]) b.remove()
-    state.layers = BrickGenerator()
+  for (const b of [...document.getElementsByClassName('brick')]) b.remove();
+  state.layers = BrickGenerator();
 
-    state.home.style.display = 'none'
-    state.over.style.display = 'none'
-    state.game.style.display = 'flex'
-    state.prompt.style.display = 'flex'
+  state.home.style.display = 'none';
+  state.over.style.display = 'none';
+  state.game.style.display = 'flex';
+  state.prompt.style.display = 'flex';
 }
 
 export function resetBall(state) {
-    state.ball.style.cssText    = ''
-    state.breaker.style.cssText = ''
-    state.waiting = true
-    state.dx = 4
-    state.dy = -4
-    state.prompt.style.display = 'flex'
+  state.ball.style.cssText = '';
+  state.breaker.style.cssText = '';
+  state.waiting = true;
+  const arr = [4, -4];
+  state.dx = arr[Math.floor(Math.random() * arr.length)];
+  state.dy = arr[Math.floor(Math.random() * arr.length)];
+  state.prompt.style.display = 'flex';
 }
